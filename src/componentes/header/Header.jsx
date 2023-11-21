0f55a61ad2d458eafe9f29aa91f852146860c6d5
SantiagoBarletta <santiagobarletta@outlook.com>
2023-11-21 03:30:25 -0300

    actualizacoin menu

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import imagenes from "../imagenes";
+import './Header.css'
 
 const Header = () => {
   return (
@@ -12,7 +13,7 @@ const Header = () => {
            src={imagenes.viernes13slider}
            alt="First slide"
          />
-        <Carousel.Caption>
+        <Carousel.Caption className='carrusel'>
           <h3>Viernes 13</h3>
           <p>Un grupo de jóvenes visita el desahuciado campamento de Crystal Lake, donde tuvieron lugar varios monstruosos asesinatos hace más de dos décadas. Allí son brutalmente atacados por Jason, el temido asesino.</p>
         </Carousel.Caption>
@@ -23,7 +24,7 @@ const Header = () => {
            src={imagenes.halloweenslider}
            alt="First slide"
          />
-        <Carousel.Caption>
+        <Carousel.Caption className='carrusel'>
           <h3>Halloween</h3>
           <p>El pequeño Michael Myers asesina a su hermana en la noche de Halloween de 1963, por lo que es internado en un psiquiátrico. Seis años más tarde, Myers se escapa del hospital y regresa a su pueblo natal, Haddonfield, en Illinois. El psicópata dará comienzo a una serie de horripilantes asesinatos mientras uno de los médicos que lo trataba en el hospital le sigue la pista.</p>
         </Carousel.Caption>
@@ -34,7 +35,7 @@ const Header = () => {
            src={imagenes.returnslider}
            alt="First slide"
          />
-        <Carousel.Caption>
+        <Carousel.Caption className='carrusel'>
           <h3>The Return of the Living Dead</h3>
           <p>
           En su primer día en un almacén de suministros médicos, Freddy (Thom Mathews) libera, sin querer, gas tóxico de un barco militar secreto de EE. UU. El gas reanima a un ejército de cadáveres con un hambre voraz ¡por cerebros humanos!
