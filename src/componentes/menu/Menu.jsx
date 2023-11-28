4e67562e8d752e87206d82a18983da79e7359958
SantiagoBarletta <santiagobarletta@outlook.com>
2023-11-28 20:55:19 -0300

    Agregue trailer

--- src/componentes/menu/Menu.jsx
+++ src/componentes/menu/Menu.jsx
@@ -9,14 +9,14 @@ import imagenes from "../imagenes";
 const Menu = () => {
   return (
     <Navbar expand="lg" className="bg-body-dark menunav">
-      <Navbar.Brand href="/proyecto-final" className="text-white">
+      <Navbar.Brand href="/proyecto-final/" className="text-white">
         <img src= {imagenes.logo} className="logomenu"></img>
 
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <Link to="/proyecto-final" className="linkMenu text-white ">
+          <Link to="/proyecto-final/" className="linkMenu text-white ">
             Inico
           </Link>
           <Link to="/peliculas" className="linkMenu text-white">
@@ -25,18 +25,6 @@ const Menu = () => {
           <Link to="/milista" className="linkMenu text-white">
             Mi lista
           </Link>
-
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/componentes/sliderpeliculas/DetallePeliculaSlider.jsx
+++ src/componentes/sliderpeliculas/DetallePeliculaSlider.jsx
@@ -2,47 +2,86 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import './SliderPeliculas.css'
 
+
+
+
 function DetallePeliculaSlider() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [pelicula, setPelicula] = useState({});
+  const [trailerKey, setTrailerKey] = useState(null);
 
   useEffect(() => {
-    const API_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=dd00aa6b89a4eaf22b5fbf601827b192&language=es-ES`;
-
-    const fetchData = async () => {
+    const fetchMovieDetails = async () => {
+      const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=dd00aa6b89a4eaf22b5fbf601827b192&language=es-ES`;
+      const trailerUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=dd00aa6b89a4eaf22b5fbf601827b192&language=es-ES`;
+  
       try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        setPelicula(data);
+        const [detailsResponse, trailerResponse] = await Promise.all([
+          fetch(movieDetailsUrl),
+          fetch(trailerUrl)
+        ]);
+  
+        const detailsData = await detailsResponse.json();
+        const trailerData = await trailerResponse.json();
+  
+        setPelicula(detailsData);
+  
+        // Buscar el primer video de tipo 'Trailer' en la respuesta de la API de trailers
+        const trailer = trailerData.results.find(video => video.type === 'Trailer');
+  
+        // Si se encuentra un trailer, establecer el estado del enlace del trailer
+        if (trailer) {
+          setTrailerKey(trailer.key);
+        }
+  
         setLoading(false);
       } catch (error) {
         console.error(error);
         setLoading(false);
       }
     };
-
-    fetchData();
+  
+    fetchMovieDetails();
   }, [id]);
+  
 
   return (
     <div>
       {loading ? (
-        <p>Cargando Detalle de la Película...</p>
-      ) : (
-        <><div className="titulo"> <p>{pelicula.original_title}</p></div>
-          <div className="contenedor-detalles">
-            
-            <div class="poster">
-              <img src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`} alt={pelicula.original_title} />
-            </div>
+  <p>Cargando Detalle de la Película...</p>
+) : (
+  <>
+    <div className="titulo">
+      <p>{pelicula.original_title}</p>
+    </div>
+    <div className="contenedor-detalles">
+      <div className="poster">
+        <img src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`} alt={pelicula.original_title} />
+      </div>
+      <div className="datos">
+        <h4>Sinopsis:</h4>
+        <p>{pelicula.overview}</p>
+        <p>Puntaje: {pelicula.vote_average}</p>
+        {trailerKey && (
+          <div className="trailer">
+            <p>Trailer:</p>
+            <iframe
+              width="800"
+              height="600"
+              src={`https://www.youtube.com/embed/${trailerKey}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        )}
+      </div>
+    </div>
+  </>
+)}
 
-            <div className="datos">
-              <h4>Sinopsis:</h4><p> {pelicula.overview}</p>
-              <p>Puntaje: {pelicula.vote_average}</p>
-            </div>
-          </div></>
-      )}
     </div>
   );
 }
